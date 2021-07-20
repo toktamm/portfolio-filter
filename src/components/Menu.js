cd09cd294df303314affcf1f32655c0560eee6bd
Toktam <ttmohebbi@gmail.com>
2021-07-20 19:53:10 +0000

    add more style

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,11 +2,11 @@ import React from "react";
 
 export default function Menu({ menuItem }) {
   return (
-    <div className="menu">
+    <div className="menu-menu">
       {menuItem.map((item) => {
         return (
-          <div className="item-container" key={item.id}>
-            <div className="item">
+          <div className="menu-item-container" key={item.id}>
+            <div className="menu-item">
               <img src={item.image} alt="blog post image" />
               <h2>{item.title}</h2>
               <p>{item.description}</p>
