6796efadb3f5c81e0c95683115ab47cab1a019e5
Toktam <ttmohebbi@gmail.com>
2021-07-20 19:30:09 +0000

    show menu items

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function Menu({ menuItem }) {
   return (
     <div>
-
+      {menuItem.map((item) => {
+        return (
+          <div className="item-container" key={item.id}>
+            <div className="item">
+              <img src={item.image} alt="blog post image" />
+              <h2>{item.title}</h2>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
